3e2ee952c3d85bab8a2021643118833cd6e8b84c
Bahattin Cinic <bahattin@adphorus.com>
2018-02-01 22:16:45 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='apistar_shell',
-    version='0.1',
+    version='1.0',
     packages=['apistar_shell'],
     url='https://github.com/bahattincinic/apistar_shell',
     license='BSD',
@@ -15,11 +15,8 @@ setup(
     author_email='bahattincinic@gmail.com',
     install_requires=['apistar', 'ipython'],
     classifiers=[
-        'Development Status :: 1 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
